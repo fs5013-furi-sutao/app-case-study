4b97dbbef88e159deea928f0abe29e3c0ca3cc09
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2022-03-01 01:17:08 +0900

    gatsby-plugin-google-analytics を削除

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,12 +88,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     "gatsby-plugin-theme-ui",
     `gatsby-plugin-netlify-cms`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: settings.ga,
-      },
-    },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
